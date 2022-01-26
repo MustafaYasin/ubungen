b04b5b58b34df7037415e28e0b5636a058888c1f
Mustafa Yasin <myasin@adobe.com>
2022-01-26 14:06:06 +0100

    changing to ubungen

---
+++ ubungen/pipelines.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+from scrapy.exporters import CsvItemExporter
+
+from imdb.utils.contentpath import export_order
+
+
+class ImdbPipeline(object):
+    def open_spider(self, spider):
+        self.file = open("data.csv", 'wb')
+        self.exporter = CsvItemExporter(self.file)
+        self.exporter.fields_to_export = export_order
+        self.exporter.start_exporting()
+
+    def process_item(self, item, spider):
+        self.exporter.export_item(item)
+        return item
+
+    def close_spider(self, spider):
+        self.exporter.finish_exporting()
+        self.file.close()
---
+++ ubungen/spiders/ubungen_spider.py
@@ -0,0 +1,34 @@
+import re
+import scrapy
+from scrapy.loader import ItemLoader
+from imdb.items import ImdbItem
+from imdb.utils.contentpath import name_to_xpath_mapper
+
+
+class IMDBSpider(scrapy.Spider):
+    name = "imdb"
+    start_urls = ["http://www.imdb.com/chart/top"]
+
+    def __init__(self, min_votes = 1000, *args, **kwargs):
+        super(IMDBSpider, self).__init__(*args, **kwargs)
+        self.min_votes = int(min_votes)
+
+    def parse(self, response):
+        for genre_page in response.xpath('//li[@class = "subnav_item_main"]/a/@href').extract():
+            genre_page = re.sub(r'(num_votes=)\d+', '\g<1>{}'.format(self.min_votes), genre_page)
+            yield scrapy.Request(response.urljoin(genre_page), callback=self.parse_attributes)
+
+    def parse_attributes(self, response):
+        for movie_url in response.xpath('//h3[@class = "lister-item-header"]/a/@href').extract():
+            yield scrapy.Request(response.urljoin(movie_url), callback=self.fetch_movie)
+
+    def fetch_movie(self, response):
+        item_loader = ItemLoader(item=ImdbItem(), response=response)
+
+        for name, xpath in name_to_xpath_mapper.items():
+            item_loader.add_xpath(name, xpath)
+
+        yield item_loader.load_item()
+        next_page = response.xpath('//div[@class = "nav"]/div[@class = "desc"]/a/@href').extract_first()
+        if next_page:
+            yield scrapy.Request(response.urljoin(next_page), callback= self.fetch_movie)

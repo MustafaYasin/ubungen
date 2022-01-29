ad0f1c080ac34d41eaa8803772c10ff7381cc77a
Mustafa Yasin <myasin@adobe.com>
2022-01-29 22:16:40 +0100

    call_exercise method

--- ubungen/spiders/ubungen_spider.py
+++ ubungen/spiders/ubungen_spider.py
@@ -1,5 +1,6 @@
 import re
 import scrapy
+import pprint
 from scrapy.loader import ItemLoader
 from ubungen.items import UbungenItem
 from ubungen.utils.contentpath import name_to_xpath_mapper
@@ -23,31 +24,36 @@ class UbungenSpider(scrapy.Spider):
 
     def parse_list_of_exercises(self, response):
         for exercise in response.xpath('//h3[@class="content-list-title"]/a/@href').extract():
+
             yield scrapy.Request(exercise, callback=self.call_exercises)
 
-    '''
-    def fetch_movie(self, response):
-        item_loader = ItemLoader(item=UbugenItem(), response=response)
 
-        for name, xpath in name_to_xpath_mapper.items():
-            item_loader.add_xpath(name, xpath)
 
-        yield item_loader.load_item()
-        next_page = response.xpath('//div[@class = "nav"]/div[@class = "desc"]/a/@href').extract_first()
-        if next_page:
-            yield scrapy.Request(response.urljoin(next_page), callback= self.fetch_movie)
-    '''
-        
-    
     def call_exercises(self, response):
-        muscle_group = response.xpath('//nav[@class="breadcrumb"]/a/text()').extract_first()
-        subset_muscles = response.xpath('//nav[@class="breadcrumb"]/span/a/span/text()').extract()[1]
-        exercise_title = response.xpath('//h1[@class="entry-title"]/text()').extract_first()
-        muscle_description_title = response.xpath('//div[@class="entry-content clearfix"]/h2/text()').extract_first()
-        TAG_RE = re.compile(r'<[^>]+>')
-        muscle_description = response.xpath('//div[@class="entry-content clearfix"]/p').extract_first()
-        muscle_description = TAG_RE.sub('', muscle_description)
-        exercise_execution_title = response.xpath('//div[@class="entry-content clearfix"]/h2/text()').extract()[1]
-        #exercise_execution = 
+        name_to_xpath_mapper = {
+            'muscle_group': 'response.xpath(\'//nav[@class="breadcrumb"]/a/text()\').extract_first()',
+            'subset_muscles': 'response.xpath(\'//nav[@class="breadcrumb"]/span/a/span/text()\').extract()[1]',
+            'exercise_title': 'response.xpath(\'//h1[@class="entry-title"]/text()\').extract_first()',
+            'muscle_description_title': 'response.xpath(\'//div[@class="entry-content clearfix"]/h2/text()\').extract_first()',
+            'muscle_description': 'response.xpath(\'//div[@class="entry-content clearfix"]/p\').extract_first()',
+            'exercise_execution_title': 'response.xpath(\'//div[@class="entry-content clearfix"]/h2/text()\').extract()[1]',
+
+        }
+
+
+        export_order = ["movie_name", "muscle_group", "subset_muscles", "exercise_title", "muscle_description_title",
+                        "muscle_description", "exercise_execution_title"]
+        for xpath in name_to_xpath_mapper.values():
+            yield {
+                'muscle_group': xpath,
+                #'subset_muscles' : response.xpath('//nav[@class="breadcrumb"]/span/a/span/text()').extract()[1]
+                #name : response.xpath('//h1[@class="entry-title"]/text()').extract_first()
+                #name : response.xpath('//div[@class="entry-content clearfix"]/h2/text()').extract_first()
+                #name : response.xpath('//div[@class="entry-content clearfix"]/p').extract_first()
+
+            }
+
+
+
 
 
--- ubungen/utils/contentpath.py
+++ ubungen/utils/contentpath.py
@@ -1,11 +1,13 @@
+
 name_to_xpath_mapper = {
-    'muscle_group': '//h1[@itemprop = "name"]/text()',
-    'subset_muscles': '//span[@id = "titleYear"]/a/text()',
-    'exercise_title': '//span[@itemprop="ratingValue"]/text()',
-    'muscle_description_title': '//span[@itemprop="ratingCount"]/text()',
-    'muscle_description': '//span[@itemprop="director"]/a/span/text()',
-    'exercise_execution_title': '//span[@itemprop="actors"]/a/span/text()',
+    'movie_name': '//nav[@class="breadcrumb"]/a/text()',
+    'muscle_group': '//nav[@class="breadcrumb"]/a/text()',
+    'subset_muscles': '//nav[@class="breadcrumb"]/span/a/span/text()',
+    'exercise_title': '//h1[@class="entry-title"]/text()',
+    'muscle_description_title': '//div[@class="entry-content clearfix"]/h2/text()',
+    'muscle_description': '//div[@class="entry-content clearfix"]/p',
+    'exercise_execution_title': '//div[@class="entry-content clearfix"]/h2/text()',
 
 }
 
-export_order = ["muscle_group", "subset_muscles", "exercise_title", "muscle_description_title", "muscle_description", "exercise_execution_title"]
+export_order = ["movie_name", "muscle_group", "subset_muscles", "exercise_title", "muscle_description_title", "muscle_description", "exercise_execution_title"]
